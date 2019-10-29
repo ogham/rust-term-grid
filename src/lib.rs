0be5b0f72dbef9350a3a99ebf5ab75b741d45999
Pierre Peltier <pierre.peltier@adevinta.com>
2019-10-29 14:51:01 +0100

    Move to the 2018 edition

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,13 @@
 #![crate_name = "term_grid"]
 #![crate_type = "rlib"]
 #![crate_type = "dylib"]
-
 #![deny(unsafe_code)]
-
 #![warn(missing_copy_implementations)]
 #![warn(missing_debug_implementations)]
 #![warn(missing_docs)]
 #![warn(trivial_numeric_casts)]
 #![warn(unreachable_pub)]
 #![warn(unused_results)]
-
 
 //! This library arranges textual data in a grid format suitable for
 //! fixed-width fonts, using an algorithm to minimise the amount of space
@@ -95,16 +92,14 @@
 //! `Cell` values are public, meaning you can construct your own instances as
 //! necessary.
 
-
 use std::cmp::max;
-use std::convert;
 use std::cmp::{Ord, Ordering};
+use std::convert;
 use std::fmt;
 use std::iter::repeat;
 
 extern crate unicode_width;
 use unicode_width::UnicodeWidthStr;
-
 
 /// A **Cell** is the combination of a string and its pre-computed length.
 ///
@@ -113,7 +108,6 @@ use unicode_width::UnicodeWidthStr;
 /// However, the fields are public, if you wish to provide your own length.
 #[derive(Debug, Clone)]
 pub struct Cell {
-
     /// The string to display when this cell gets rendered.
     pub contents: String,
 
@@ -162,7 +156,6 @@ impl Eq for Cell {}
 /// Direction cells should be written in - either across, or downwards.
 #[derive(PartialEq, Debug, Copy, Clone)]
 pub enum Direction {
-
     /// Starts at the top left and moves rightwards, going back to the first
     /// column for a new row - like a typewriter.
     LeftToRight,
@@ -172,15 +165,12 @@ pub enum Direction {
     TopToBottom,
 }
 
-
 /// The width of a cell, in columns.
 pub type Width = usize;
-
 
 /// The text to put in between each pair of columns.
 #[derive(PartialEq, Debug)]
 pub enum Filling {
-
     /// A certain number of spaces.
     Spaces(Width),
 
@@ -191,7 +181,7 @@ pub enum Filling {
 impl Filling {
     fn width(&self) -> Width {
         match *self {
-            Filling::Spaces(w)   => w,
+            Filling::Spaces(w) => w,
             Filling::Text(ref t) => UnicodeWidthStr::width(&t[..]),
         }
     }
@@ -201,7 +191,6 @@ impl Filling {
 /// `Grid::new()`.
 #[derive(PartialEq, Debug)]
 pub struct GridOptions {
-
     /// Direction that the cells should be written in - either across, or
     /// downwards.
     pub direction: Direction,
@@ -210,10 +199,8 @@ pub struct GridOptions {
     pub filling: Filling,
 }
 
-
 #[derive(PartialEq, Debug)]
 struct Dimensions {
-
     /// The number of lines in the grid.
     num_lines: Width,
 
@@ -226,15 +213,13 @@ impl Dimensions {
     fn total_width(&self, separator_width: Width) -> Width {
         if self.widths.is_empty() {
             0
-        }
-        else {
+        } else {
             let values = self.widths.iter().sum::<Width>();
             let separators = separator_width * (self.widths.len() - 1);
             values + separators
         }
     }
 }
-
 
 /// Everything needed to format the cells with the grid options.
 ///
@@ -249,12 +234,16 @@ pub struct Grid {
 }
 
 impl Grid {
-
     /// Creates a new grid view with the given options.
     pub fn new(options: GridOptions) -> Grid {
         let cells = Vec::new();
-        Grid { options, cells, widest_cell_length: 0,
-               width_sum: 0, cell_count: 0 }
+        Grid {
+            options,
+            cells,
+            widest_cell_length: 0,
+            width_sum: 0,
+            cell_count: 0,
+        }
     }
 
     /// Reserves space in the vector for the given number of additional cells
@@ -279,18 +268,17 @@ impl Grid {
     /// Returns `None` if any of the cells has a width greater than the
     /// maximum width.
     pub fn fit_into_width(&self, maximum_width: Width) -> Option<Display> {
-        self.width_dimensions(maximum_width)
-            .map(|dims| Display {
-                grid:       self,
-                dimensions: dims,
-            })
+        self.width_dimensions(maximum_width).map(|dims| Display {
+            grid: self,
+            dimensions: dims,
+        })
     }
 
     /// Returns a displayable grid with the given number of columns, and no
     /// maximum width.
     pub fn fit_into_columns(&self, num_columns: usize) -> Display {
         Display {
-            grid:       self,
+            grid: self,
             dimensions: self.columns_dimensions(num_columns),
         }
     }
@@ -308,8 +296,8 @@ impl Grid {
         let mut widths: Vec<Width> = repeat(0).take(num_columns).collect();
         for (index, cell) in self.cells.iter().enumerate() {
             let index = match self.options.direction {
-                Direction::LeftToRight  => index % num_columns,
-                Direction::TopToBottom  => index / num_lines,
+                Direction::LeftToRight => index % num_columns,
+                Direction::TopToBottom => index / num_lines,
             };
             widths[index] = max(widths[index], cell.width);
         }
@@ -352,12 +340,18 @@ impl Grid {
         }
 
         if self.cell_count == 0 {
-            return Some(Dimensions { num_lines: 0, widths: Vec::new() });
+            return Some(Dimensions {
+                num_lines: 0,
+                widths: Vec::new(),
+            });
         }
 
         if self.cell_count == 1 {
             let the_cell = &self.cells[0];
-            return Some(Dimensions { num_lines: 1, widths: vec![ the_cell.width ] });
+            return Some(Dimensions {
+                num_lines: 1,
+                widths: vec![the_cell.width],
+            });
         }
 
         let theoretical_max_num_lines = self.theoretical_max_num_lines(maximum_width);
@@ -369,14 +363,18 @@ impl Grid {
                 // I clone self.cells twice. Once here, and once in
                 // self.theoretical_max_num_lines. Perhaps not the best for
                 // performance?
-                widths: self.cells.clone().into_iter().map(|cell| cell.width).collect()
+                widths: self
+                    .cells
+                    .clone()
+                    .into_iter()
+                    .map(|cell| cell.width)
+                    .collect(),
             });
         }
         // Instead of numbers of columns, try to find the fewest number of *lines*
         // that the output will fit in.
         let mut smallest_dimensions_yet = None;
-        for num_lines in (1 .. theoretical_max_num_lines).rev() {
-
+        for num_lines in (1..theoretical_max_num_lines).rev() {
             // The number of columns is the number of cells divided by the number
             // of lines, *rounded up*.
             let mut num_columns = self.cell_count / num_lines;
@@ -410,12 +408,9 @@ impl Grid {
     }
 }
 
-
-
 /// A displayable representation of a Grid.
 #[derive(PartialEq, Debug)]
 pub struct Display<'grid> {
-
     /// The grid to display.
     grid: &'grid Grid,
 
@@ -424,11 +419,11 @@ pub struct Display<'grid> {
 }
 
 impl<'grid> Display<'grid> {
-
     /// Returns how many columns this display takes up, based on the separator
     /// width and the number and width of the columns.
     pub fn width(&self) -> Width {
-        self.dimensions.total_width(self.grid.options.filling.width())
+        self.dimensions
+            .total_width(self.grid.options.filling.width())
     }
 
     /// Returns how many rows this display takes up.
@@ -450,11 +445,11 @@ impl<'grid> Display<'grid> {
 
 impl<'grid> fmt::Display for Display<'grid> {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        for y in 0 .. self.dimensions.num_lines {
-            for x in 0 .. self.dimensions.widths.len() {
+        for y in 0..self.dimensions.num_lines {
+            for x in 0..self.dimensions.widths.len() {
                 let num = match self.grid.options.direction {
-                    Direction::LeftToRight  => y * self.dimensions.widths.len() + x,
-                    Direction::TopToBottom  => y + self.dimensions.num_lines * x,
+                    Direction::LeftToRight => y * self.dimensions.widths.len() + x,
+                    Direction::TopToBottom => y + self.dimensions.num_lines * x,
                 };
 
                 // Abandon a line mid-way through if that's where the cells end
@@ -465,29 +460,27 @@ impl<'grid> fmt::Display for Display<'grid> {
                 let cell = &self.grid.cells[num];
                 if x == self.dimensions.widths.len() - 1 {
                     // The final column doesn't need to have trailing spaces
-                    try!(write!(f, "{}", cell.contents));
-                }
-                else {
+                    write!(f, "{}", cell.contents)?;
+                } else {
                     assert!(self.dimensions.widths[x] >= cell.width);
                     match self.grid.options.filling {
                         Filling::Spaces(n) => {
                             let extra_spaces = self.dimensions.widths[x] - cell.width + n;
-                            try!(write!(f, "{}", pad_string(&cell.contents, extra_spaces)));
-                        },
+                            write!(f, "{}", pad_string(&cell.contents, extra_spaces))?;
+                        }
                         Filling::Text(ref t) => {
                             let extra_spaces = self.dimensions.widths[x] - cell.width;
-                            try!(write!(f, "{}{}", pad_string(&cell.contents, extra_spaces), t));
-                        },
+                            write!(f, "{}{}", pad_string(&cell.contents, extra_spaces), t)?;
+                        }
                     }
                 }
             }
-            try!(write!(f, "\n"));
+            write!(f, "\n")?;
         }
 
         Ok(())
     }
 }
-
 
 /// Pad a string with the given number of spaces.
 fn spaces(length: usize) -> String {
@@ -502,7 +495,6 @@ fn pad_string(string: &str, padding: usize) -> String {
     format!("{}{}", string, spaces(padding))
 }
 
-
 #[cfg(test)]
 mod test {
     use super::*;
@@ -510,8 +502,8 @@ mod test {
     #[test]
     fn no_items() {
         let grid = Grid::new(GridOptions {
-            direction:  Direction::TopToBottom,
-            filling:    Filling::Spaces(2),
+            direction: Direction::TopToBottom,
+            filling: Filling::Spaces(2),
         });
 
         let display = grid.fit_into_width(40).unwrap();
@@ -525,8 +517,8 @@ mod test {
     #[test]
     fn one_item() {
         let mut grid = Grid::new(GridOptions {
-            direction:  Direction::TopToBottom,
-            filling:    Filling::Spaces(2),
+            direction: Direction::TopToBottom,
+            filling: Filling::Spaces(2),
         });
 
         grid.add(Cell::from("1"));
@@ -534,7 +526,7 @@ mod test {
         let display = grid.fit_into_width(40).unwrap();
 
         assert_eq!(display.dimensions.num_lines, 1);
-        assert_eq!(display.dimensions.widths, vec![ 1 ]);
+        assert_eq!(display.dimensions.widths, vec![1]);
 
         assert_eq!(display.width(), 1);
     }
@@ -542,8 +534,8 @@ mod test {
     #[test]
     fn one_item_exact_width() {
         let mut grid = Grid::new(GridOptions {
-            direction:  Direction::TopToBottom,
-            filling:    Filling::Spaces(2),
+            direction: Direction::TopToBottom,
+            filling: Filling::Spaces(2),
         });
 
         grid.add(Cell::from("1234567890"));
@@ -551,7 +543,7 @@ mod test {
         let display = grid.fit_into_width(10).unwrap();
 
         assert_eq!(display.dimensions.num_lines, 1);
-        assert_eq!(display.dimensions.widths, vec![ 10 ]);
+        assert_eq!(display.dimensions.widths, vec![10]);
 
         assert_eq!(display.width(), 10);
     }
@@ -559,8 +551,8 @@ mod test {
     #[test]
     fn one_item_just_over() {
         let mut grid = Grid::new(GridOptions {
-            direction:  Direction::TopToBottom,
-            filling:    Filling::Spaces(2),
+            direction: Direction::TopToBottom,
+            filling: Filling::Spaces(2),
         });
 
         grid.add(Cell::from("1234567890!"));
@@ -571,8 +563,8 @@ mod test {
     #[test]
     fn two_small_items() {
         let mut grid = Grid::new(GridOptions {
-            direction:  Direction::TopToBottom,
-            filling:    Filling::Spaces(2),
+            direction: Direction::TopToBottom,
+            filling: Filling::Spaces(2),
         });
 
         grid.add(Cell::from("1"));
@@ -581,7 +573,7 @@ mod test {
         let display = grid.fit_into_width(40).unwrap();
 
         assert_eq!(display.dimensions.num_lines, 1);
-        assert_eq!(display.dimensions.widths, vec![ 1, 1 ]);
+        assert_eq!(display.dimensions.widths, vec![1, 1]);
 
         assert_eq!(display.width(), 1 + 2 + 1);
     }
@@ -589,8 +581,8 @@ mod test {
     #[test]
     fn two_medium_size_items() {
         let mut grid = Grid::new(GridOptions {
-            direction:  Direction::TopToBottom,
-            filling:    Filling::Spaces(2),
+            direction: Direction::TopToBottom,
+            filling: Filling::Spaces(2),
         });
 
         grid.add(Cell::from("hello there"));
@@ -599,7 +591,7 @@ mod test {
         let display = grid.fit_into_width(40).unwrap();
 
         assert_eq!(display.dimensions.num_lines, 1);
-        assert_eq!(display.dimensions.widths, vec![ 11, 18 ]);
+        assert_eq!(display.dimensions.widths, vec![11, 18]);
 
         assert_eq!(display.width(), 11 + 2 + 18);
     }
@@ -607,12 +599,16 @@ mod test {
     #[test]
     fn two_big_items() {
         let mut grid = Grid::new(GridOptions {
-            direction:  Direction::TopToBottom,
-            filling:    Filling::Spaces(2),
+            direction: Direction::TopToBottom,
+            filling: Filling::Spaces(2),
         });
 
-        grid.add(Cell::from("nuihuneihsoenhisenouiuteinhdauisdonhuisudoiosadiuohnteihaosdinhteuieudi"));
-        grid.add(Cell::from("oudisnuthasuouneohbueobaugceoduhbsauglcobeuhnaeouosbubaoecgueoubeohubeo"));
+        grid.add(Cell::from(
+            "nuihuneihsoenhisenouiuteinhdauisdonhuisudoiosadiuohnteihaosdinhteuieudi",
+        ));
+        grid.add(Cell::from(
+            "oudisnuthasuouneohbueobaugceoduhbsauglcobeuhnaeouosbubaoecgueoubeohubeo",
+        ));
 
         assert_eq!(grid.fit_into_width(40), None);
     }
@@ -620,12 +616,14 @@ mod test {
     #[test]
     fn that_example_from_earlier() {
         let mut grid = Grid::new(GridOptions {
-            filling:    Filling::Spaces(1),
-            direction:  Direction::LeftToRight,
+            filling: Filling::Spaces(1),
+            direction: Direction::LeftToRight,
         });
 
-        for s in vec!["one", "two", "three", "four", "five", "six", "seven",
-                      "eight", "nine", "ten", "eleven", "twelve"] {
+        for s in vec![
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+            "eleven", "twelve",
+        ] {
             grid.add(Cell::from(s));
         }
 
@@ -637,8 +635,8 @@ mod test {
     #[test]
     fn huge_separator() {
         let mut grid = Grid::new(GridOptions {
-            filling:    Filling::Spaces(100),
-            direction:  Direction::LeftToRight,
+            filling: Filling::Spaces(100),
+            direction: Direction::LeftToRight,
         });
 
         grid.add("a".into());
@@ -650,8 +648,8 @@ mod test {
     #[test]
     fn huge_yet_unused_separator() {
         let mut grid = Grid::new(GridOptions {
-            filling:    Filling::Spaces(100),
-            direction:  Direction::LeftToRight,
+            filling: Filling::Spaces(100),
+            direction: Direction::LeftToRight,
         });
 
         grid.add("abcd".into());
@@ -659,7 +657,7 @@ mod test {
         let display = grid.fit_into_width(99).unwrap();
 
         assert_eq!(display.dimensions.num_lines, 1);
-        assert_eq!(display.dimensions.widths, vec![ 4 ]);
+        assert_eq!(display.dimensions.widths, vec![4]);
 
         assert_eq!(display.width(), 4);
     }
